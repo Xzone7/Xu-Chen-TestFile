f0fb2dfd4a950d4af319925246bc1e992e8393b1
Xu Chen <cxghy2011@gmail.com>
2016-05-24 13:27:46 +0800

    TableView

---
+++ myApp/myApp/TableSource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UIKit;
+
+namespace myApp
+{
+	public class TableSource : UITableViewSource
+	{
+		string[] tableItems;
+		string cellIdentifier = "TableCell";
+
+
+
+		public TableSource (string[] items)
+		{
+			tableItems = items;
+		}
+
+		public override nint RowsInSection (UITableView tableview, nint section)
+		{
+			return tableItems.Length;
+		}
+
+		public override void RowSelected (UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			new UIAlertView ("Alert", "You touched: " + tableItems [indexPath.Row], null, "OK", null).Show ();
+			tableView.DeselectRow (indexPath, true);
+		}
+
+		public override UITableViewCell GetCell (UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
+			if (cell == null)
+				cell = new UITableViewCell (UITableViewCellStyle.Subtitle, cellIdentifier);
+			cell.TextLabel.Text = tableItems [indexPath.Row];
+
+			if (indexPath.Row > 0)
+				cell.DetailTextLabel.Text = tableItems [indexPath.Row - 1];
+			
+			return cell;
+		}
+	}
+}
+
---
+++ myApp/myApp/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using UIKit;
+using System.Collections.Generic;
+
+namespace myApp
+{
+	public partial class ViewController : UIViewController // ViewController inherits from UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			string[] data = new string[]{ "Food", "View", "Music", "Love","Food", "View", "Music", "Love","Food", "View", "Music", "Love" };
+
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			UITableView _table;
+
+			_table = new UITableView {
+
+				Frame = new CoreGraphics.CGRect (0, 30, View.Bounds.Width, View.Bounds.Height - 30),
+				Source = new TableSource(data)
+
+			};
+
+			View.AddSubview (_table);
+
+
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
